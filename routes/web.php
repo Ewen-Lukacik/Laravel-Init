68c326ba5ed9d2deefae3f2b9ced6bdc74e68ea1
Ewen Lukacik <newelukacik7360@gmail.com>
2025-03-14 15:24:27 +0100

    Feat: added new routes for vehicles, species and comment updates

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\BlogController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\MovieController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\SpecieController;
+use App\Http\Controllers\VehicleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,9 +15,12 @@ Route::get('/', function () {
 Route::get("/index/{page}", [BlogController::class, 'index'])->name('blog.home');
 Route::get("/people/{id}", [PostController::class, 'readPost'])->name('blog.people');
 Route::get("/films/{id}", [MovieController::class, "readMovie"])->name('blog.movie');
+Route::get("/vehicles/{id}", [VehicleController::class, "readVehicle"])->name('blog.vehicle');
+Route::get("/species/{id}", [SpecieController::class, "readSpecies"])->name('blog.species');
 
 
 //form route
 Route::post('/comment', [CommentController::class, 'create'])->name('comment.create');
 Route::delete('/comment', [CommentController::class, 'delete'])->name('comment.delete');
 Route::get('/comment/edit/{id}', [CommentController::class, 'edit'])->name('comment.edit');
+Route::post('/comment/edit/{id}', [CommentController::class, 'update'])->name('comment.update');
